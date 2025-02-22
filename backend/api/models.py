38658375abdaadb6f93eddc8b9a908fb1496afbd
Sofia <sonechka68zvr@gmail.com>
2025-02-22 17:24:50 +0300

    added models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        (1, 'Низкий'),
+        (2, 'Средний'),
+        (3, 'Высокий'),
+    ]
+
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    priority = models.IntegerField(choices=PRIORITY_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
